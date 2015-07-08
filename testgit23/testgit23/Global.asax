2d8e192244e6e16622d0ed5aa390098aea33c476
emksaz <davidz@survey.best>
2015-07-08 16:42:19 +0800

    init v1.0

---
+++ testgit23/testgit23/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testgit23.MvcApplication" Language="C#" %>
---
+++ testgit23/testgit23/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testgit23.Startup))]
+namespace testgit23
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
